91cebb17cfaf9af1675f3348c2917f584afebd6a
jbllona <jesse.llona@gmail.com>
2017-03-11 13:48:43 -0800

    now using threading and accepting server-side user commands

--- GreetingServer.java
+++ GreetingServer.java
@@ -1,10 +1,61 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
+
+class WaitForInput extends Thread
+{
+	Scanner input;
+	Thread serverThread;
+	public WaitForInput(int port, int timeOut)
+	{
+		input = new Scanner(System.in);
+		
+		serverThread = null;
+		try
+		{
+			serverThread = new GreetingServer(port, timeOut);
+			serverThread.start();
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+	
+	public WaitForInput(int port)
+	{
+		input = new Scanner(System.in);
+		
+		serverThread = null;
+		try
+		{
+			serverThread = new GreetingServer(port);
+			serverThread.start();
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+	
+	public void run()
+	{
+		String userCommand = input.next();
+		if(userCommand.equalsIgnoreCase("exit"))
+		{
+			System.out.println("attempting to exit");
+			serverThread.stop();
+			System.exit(0);
+		}
+	}
+}
 
 public class GreetingServer extends Thread
 {
 	String msgToSend = "I don't know why you say goodbye I say hello!";
 	private ServerSocket serverSocket;
+	
 	
 	public GreetingServer(int port, int timeOut) throws IOException
 	{
@@ -12,47 +63,51 @@ public class GreetingServer extends Thread
 		serverSocket.setSoTimeout(timeOut);
 	}
 	
+	public GreetingServer(int port) throws IOException
+	{
+		serverSocket = new ServerSocket(port);
+	}
+	
 	public void run()
 	{
-		while(true)
+		try
 		{
-			try
-			{
-				System.out.println("You (to self): Wondering if after all these years I'll meet the client port");
-				Socket server = serverSocket.accept();
-				
-				DataInputStream in = new DataInputStream(server.getInputStream());
-				System.out.println("Client: " + in.readUTF());
-				
-				System.out.println("You: " + msgToSend);
-				DataOutputStream out = new DataOutputStream(server.getOutputStream());
-				out.writeUTF(msgToSend);
-				
-				server.close();
-			}
-			catch(SocketTimeoutException s)
-			{
-				System.out.println("Socket timed out");
-				break;
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-				break;
-			}
+			System.out.println("You (to self): Wondering if after all these years I'll meet the client port");
+			Socket server = serverSocket.accept();
+			DataInputStream in = new DataInputStream(server.getInputStream());
+			System.out.println("Client: " + in.readUTF());
+			
+			System.out.println("You: " + msgToSend);
+			DataOutputStream out = new DataOutputStream(server.getOutputStream());
+			out.writeUTF(msgToSend);
+			
+			server.close();
+		}
+		catch(SocketTimeoutException s)
+		{
+			System.out.println("Socket timed out");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args)
 	{
 		int port = Integer.parseInt(args[0]);
-		int timeOutMs = Integer.parseInt(args[1]);
+		int timeOutMs = 0;
+		if(args.length > 1)
+		{
+				timeOutMs = Integer.parseInt(args[1]);
+		}
+		
 		try
 		{
-			Thread t = new GreetingServer(port, timeOutMs);
+			Thread t = new WaitForInput(port);
 			t.start();
 		}
-		catch(IOException e)
+		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
